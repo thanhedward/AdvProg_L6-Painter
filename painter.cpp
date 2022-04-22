4057f8341a689dcc75be4233b732dca7126a207a
thanhedward <ngocthanh12082003d@gmail.com>
2022-04-22 16:06:12 +0700

    test 1

--- painter.cpp
+++ painter.cpp
@@ -9,6 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    this->color = color;
 }
 
 
@@ -21,6 +22,10 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    float rad = (angle/180) * M_PI;
+    x += cos(rad) * numPixel;
+    y -= sin(rad) * numPixel;
+
 }
 
 
@@ -33,6 +38,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    Painter::jumpForward(-numPixel);
 }
 
 
@@ -45,6 +51,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
+    setAngle(angle + degree);
 }
 
 
@@ -56,7 +63,8 @@ void Painter::turnLeft(double degree)
 ***/     
 void Painter::turnRight(double degree)
 {
-    // TODO: rotate right the painter   
+    // TODO: rotate right the painter
+    Painter::turnLeft(-degree);
 }
 
 /***  
@@ -67,7 +75,12 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    // TODO: set random color
+    Uint8 r = rand() % 256;
+    Uint8 g = rand() % 256;
+    Uint8 b = rand() % 256;
+    SDL_Color color = { r, g, b };
+    setColor(color);
 }
 
 
