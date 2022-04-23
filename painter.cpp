c1ecf34d0f6ef0534eefe9fe05ec7d9c48afe025
thanhedward <ngocthanh12082003d@gmail.com>
2022-04-23 10:22:46 +0700

    test again

--- painter.cpp
+++ painter.cpp
@@ -10,9 +10,7 @@ void Painter::setColor(SDL_Color color)
 {
     // TODO: set the color value for the Painter and set Render Draw Color
     this->color = color;
-    SDL_SetRenderDrawColor(renderer, color);
 }
-
 
 /***
     Args: numPixel (int): number of pixel for jumping forward
